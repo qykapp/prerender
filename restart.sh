f18e4946e28d97d3b4b2f4ed700d48b094aa92e2
Shobhit Singhal <shobhitsinghal624@gmail.com>
2017-01-13 11:52:31 +0530

    CACHE_ROOT_DIR in restart script

--- restart.sh
+++ restart.sh
@@ -8,6 +8,7 @@ function restart {
     OUTFILE=/var/log/prerender/prerender-$1.log
     ERRFILE=/var/log/prerender/prerender-$1.log
     SCRIPT=server.js
+    CACHE_ROOT_DIR=/data/ebs1/prerender-cache/
     PORT=$((4337 + 2 * ($1 - 1)))
 
     echo "forever stop $PROCESS_ID"
@@ -15,8 +16,8 @@ function restart {
 
     sleep 2
 
-    echo "PORT=$PORT forever start --uid=$PROCESS_ID --pidFile=$PIDFILE -a -l $LOGFILE -o $OUTFILE -e $ERRFILE $SCRIPT"
-    PORT=$PORT forever start --uid=$PROCESS_ID --pidFile=$PIDFILE -a -l $LOGFILE -o $OUTFILE -e $ERRFILE $SCRIPT
+    echo "PORT=$PORT CACHE_ROOT_DIR=$CACHE_ROOT_DIR forever start --uid=$PROCESS_ID --pidFile=$PIDFILE -a -l $LOGFILE -o $OUTFILE -e $ERRFILE $SCRIPT"
+    PORT=$PORT CACHE_ROOT_DIR=$CACHE_ROOT_DIR forever start --uid=$PROCESS_ID --pidFile=$PIDFILE -a -l $LOGFILE -o $OUTFILE -e $ERRFILE $SCRIPT
 }
 
 if [ -z "$1" ]
