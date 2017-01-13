486afdb48290f7bba39fb7753409782487c36026
Shobhit Singhal <shobhitsinghal624@gmail.com>
2017-01-13 15:50:19 +0530

    access logs

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    accessLogs: {
+      filepath: process.env.PRERENDER_ACCESS_LOGS_FILE,
+      format: process.env.PRERENDER_ACCESS_LOGS_FORMAT || ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms ":referrer" ":user-agent"',
+    },
 });
 
 
+server.use(prerender.accessLogs());
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
